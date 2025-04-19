6b3c999b0f32e56b6a63bec95fced76d995dd035
Pavel <cherepashka0805@mail.ru>
2025-04-19 15:01:38 +0300

    Добавил цветовую палитру

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
-import { render } from 'solid-js/web'
-import { Router, Route } from "@solidjs/router";
+/* @refresh reload */
+import { render } from 'solid-js/web';
 
-import WeddingInvitation from "./App";
+import './index.css';
+import App from './App';
 
+const root = document.getElementById('root');
 
-render(
-    () => (
-        <Router>
-          <Route path="/" component={WeddingInvitation} />
-        </Router>
-    ),
-    document.getElementById('root')!
-);
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+    throw new Error(
+        'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    );
+}
 
-
+render(() => <App />, root);
--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,8 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: '/invite/',
   build: {
     target: 'esnext',
-    outDir: 'dist',
   },
-  base: '/Invite/',
 });
